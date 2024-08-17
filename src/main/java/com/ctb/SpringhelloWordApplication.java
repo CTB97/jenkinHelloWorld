051f6c2b4825291b68dc81a3439575b07f1b015d
CTB97 <boyecheikhtidiane97@gmail.com>
2024-08-17 13:21:14 +0000

    first commit

---
+++ src/main/java/com/ctb/SpringhelloWordApplication.java
@@ -0,0 +1,13 @@
+package com.ctb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringhelloWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringhelloWordApplication.class, args);
+	}
+
+}
